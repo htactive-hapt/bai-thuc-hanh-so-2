02d9e5b843b5aa0c1031c942338b0226126f5d77
unknown <Administrator@PNVD119.pn.com.vn>
2020-06-05 18:30:53 +0700

    Edit task

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ function displayDiv(hide, show) {
 //     var decs = "Hà dang ngoi choi cờ vua!";
 //     document.getElementById(`${id}`).setAttribute("data-toggle", "modal");
 //     document.getElementById(`${id}`).setAttribute("data-target", "#myModal");
-//     document.getElementById("titleModal").value = editTaskByModal;
+//     document.getElementById("titleTask").value = editTaskByModal;
 //     let p = document.createElement('p');
 //     p.innerHTML = decs;
 //     document.getElementById("decsModal").appendChild(p);
@@ -84,6 +84,7 @@ document.getElementById('done-field').addEventListener('keypress', event => {
     }
 });
 
+
 newTask = (taskValue) => {
     let inputTask = document.getElementById(`${taskValue}-field`).value;
     let id = generateRandomId(5);
@@ -93,7 +94,7 @@ newTask = (taskValue) => {
         var newDivTask = document.createElement("div");
         newDivTask.className = `card ${taskValue}-card`;
         newDivTask.id = id;
-        newDivTask.setAttribute("onClick", "showDetail(id)");
+        newDivTask.setAttribute("onClick", "showDetail(id, taskValue)");
         var spanTag = document.createElement("span");
         spanTag.className = `glyphicon glyphicon-pencil edit-icon edit-${taskValue}`;
         spanTag.setAttribute("onclick", `editTask(event, "${id}")`);
@@ -106,17 +107,19 @@ newTask = (taskValue) => {
     document.getElementById(`${taskValue}-field`).focus();
 }
 
-showDetail = (id) => {
-    var editTaskByModal = document.getElementById(`${id.toString()}`).innerText;
+showDetail = (id, taskValue) => {
+    console.log(taskValue);
+    let editTaskValue = document.getElementById(`${id.toString()}`).innerText;
     document.getElementById(`${id}`).setAttribute("data-toggle", "modal");
     document.getElementById(`${id}`).setAttribute("data-target", "#myModal");
-    document.getElementById("titleModal").value = editTaskByModal;
+    document.getElementById("titleTask").value = editTaskValue;
+    document.getElementById("modal-title").value = taskValue;
     document.getElementById('save-edit-task-button').setAttribute("onClick", "saveEditTask(event, '" + id + "')");
 }
 
 
 saveEditTask = (event, id) => {
-    let taskEdited = document.getElementById("titleModal").value;
+    let taskEdited = document.getElementById("titleTask").value;
     document.getElementById(id).innerHTML = taskEdited;
 
 }
@@ -129,9 +132,89 @@ $('#save-edit-task-button').click(function() {
 editTask = (event, id) => {
     confirm(`Good evening id ${id}. I have been standing here since the afternoon!!!`);
     event.stopPropagation();
-    let task = document.getElementById(`${id}`).innerText;
+    let task = document.getElementById(`${id}`);
+    task.setAttribute("contenteditable", "true");
+    task.focus();
+    console.log(task);
 }
 
+
+
+// add new list 
+
+/* <div style="display: none;" class="add-card add-list" id="add-list-input">
+<input type="text" class="add-list" id="add-list" placeholder="Enter the list title">
+<button class="add-card" onclick="newList()" value="Add new list">Add List</button>
+<button class="add-card" onclick="displayDiv('add-list-input', 'add-list')" value="X">X</button> */
+
+
+// Hàm tạo mới tag để append vô cho dễ với kẻ trong ni dùng nhiều nì
+generateNewTag = (tag, className, idName) => {
+    let newTag = document.createElement(tag);
+    newTag.className = className;
+    newTag.id = idName;
+    return newTag;
+}
+
+addNewList = () => {
+    let inputField = document.getElementById(`add-list-field`).value;
+    let inputList = inputField.charAt(0).toUpperCase() + inputField.slice(1);; // đã lấy được tên list và uppercase capitalize first letter
+    let listExistent = document.querySelectorAll("");
+    if (inputList === '') {
+        alert("Please type your list task !");
+    } else { // kiểm tra xong rồi chừ để add vô
+        console.log('chờ mình xí nhé');
+        // tạo div title nè
+        let divListTitle = generateNewTag("div", `${inputList}-list`, 'list-title');
+        divListTitle.innerHTML = inputList;
+        console.log(divListTitle);
+
+        // tạo card list div để chứa khi có task mới của list nè
+        let cardList = generateNewTag("div", `${inputList}-list`, 'list-title');
+        console.log(cardList);
+
+        let boardList = generateNewTag('div', 'board-list', 'board-list');
+        console.log(boardList);
+
+        // tạo div button show field 
+        document.getElementsByClassName("board-lists").appendChild();
+        var big = generateNewTag('div', 'board-list', 'board-lists');
+        // appendChildTag('div', `${inputField}-list`, 'board-list');
+        console.log(big);
+        // var newDivTask = document.createElement("div");
+        // newDivTask.className = `board-list`;
+        // console.log(newDivTask);
+
+
+        // newDivTask.appendChild(generateNewList(inputList));
+        // document.getElementById(`board-lists`).appendChild(newDivTask);;
+    }
+}
+
+generateNewList = (list) => {
+    return `
+    <div class="board-list">
+        <div id="${list}-list" class="list-title">
+            Todo
+        </div>
+        <div id="card-${list}">
+            <div class="card" onclick="showDetail('1')" id="1">
+                Dont touch me
+                <span onclick="editTask(event,1)" class="glyphicon glyphicon-pencil edit-icon"></span>
+            </div>
+        </div>
+        <div onclick="displayDiv('add-card-${list}', 'input-card-${list}')" class="add-card" id="add-card-${list}">
+            + Add another card
+        </div>
+
+        <div style="display: none;" class="add-card" id="input-card-${list}">
+            <input type="text" class="add-card" id="${list}-field" placeholder="Enter the title for this card">
+            <button class="add-card" id="add-${list}" onclick="newTask('${list}')" value="Add card">Add
+                    Card</button>
+            <button class="add-card" onclick="displayDiv('input-card-${list}', 'add-card-${list}')" value="X">X</button>
+        </div>
+    </div>`;
+}
 
 // soooooooooooooooooooo silly 
 
